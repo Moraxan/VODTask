72c6439cb6fce0e92a5798bfc62d71dd6c2a3ea7
Moraxan <calletrumma@gmail.com>
2023-03-03 15:05:35 +0100

    The final countdown

--- VODTask.Common/DTOs/ClickModel.cs
+++ VODTask.Common/DTOs/ClickModel.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VODTask.Common.DTOs
+﻿namespace VODTask.Common.DTOs
 {
 	public record ClickModel(string PageType, int Id);
 	
